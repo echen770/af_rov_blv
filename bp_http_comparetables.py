e508c8d2b9bffb6171512e7f4208e2d300a996d5
Ensui Chen <echen770@gmail.com>
2023-12-07 11:44:27 -0800

    major bug fix for the compare tables function

--- bp_http_comparetables.py
+++ bp_http_comparetables.py
@@ -1,68 +1,103 @@
 import azure.functions as func
 import logging
 import json
-import base64
 import traceback
-import io
-import pandas as pd
 
 bp_compare_tables = func.Blueprint()
 
 
-def compare_tables(table1, table2):
+def replace_null_with_empty_string(row):
+    # Replace null values with empty strings in the given row
+    return {key: value if value is not None else "" for key, value in row.items()}
+
+
+def compare_rows(row1, row2):
+    # Compare each key-value pair in the rows
+    for key, value in row1.items():
+        if key != "contest_id" and row2.get(key) != value:
+            return False
+    return True
+
+
+def compare_tables(sql_table, dataverse_table):
+    # Replace null values with empty strings in both tables
+    sql_table = [replace_null_with_empty_string(row) for row in sql_table]
+    dataverse_table = [replace_null_with_empty_string(row) for row in dataverse_table]
+
     # Extract contest_ids from each table
-    ids_table1 = set(row["contest_id"] for row in table1)
-    ids_table2 = set(row["contest_id"] for row in table2)
+    ids_sql_table = set(row["contest_id"] for row in sql_table)
+    ids_dataverse_table = set(row["contest_id"] for row in dataverse_table)
 
-    # Rows only in table1
-    only_in_table1 = [row for row in table1 if row["contest_id"] not in ids_table2]
+    # Rows only in sql_table
+    only_in_sql_table = [
+        row for row in sql_table if row["contest_id"] not in ids_dataverse_table
+    ]
 
-    # Rows only in table2
-    only_in_table2 = [row for row in table2 if row["contest_id"] not in ids_table1]
+    # Rows only in dataverse_table
+    only_in_dataverse_table = [
+        row for row in dataverse_table if row["contest_id"] not in ids_sql_table
+    ]
 
     # Rows in both tables but with different values
+    # common_rows_diff_values = [
+    #     row
+    #     for row in sql_table
+    #     if row in dataverse_table
+    #     and row
+    #     != next(
+    #         (r for r in dataverse_table if r["contest_id"] == row["contest_id"]), None
+    #     )
+    # ]
+
     common_rows_diff_values = [
-        row
-        for row in table1
-        if row in table2
-        and row
-        != next((r for r in table2 if r["contest_id"] == row["contest_id"]), None)
+        row1
+        for row1 in sql_table
+        for row2 in dataverse_table
+        if row1["contest_id"] == row2["contest_id"] and not compare_rows(row1, row2)
     ]
 
     # Rows in both tables that are the same
+    # common_rows_same_values = [
+    #     row
+    #     for row in sql_table
+    #     if row in dataverse_table
+    #     and row
+    #     == next(
+    #         (r for r in dataverse_table if r["contest_id"] == row["contest_id"]), None
+    #     )
+    # ]
     common_rows_same_values = [
-        row
-        for row in table1
-        if row in table2
-        and row
-        == next((r for r in table2 if r["contest_id"] == row["contest_id"]), None)
+        row1
+        for row1 in sql_table
+        for row2 in dataverse_table
+        if row1["contest_id"] == row2["contest_id"] and compare_rows(row1, row2)
     ]
 
     return (
-        only_in_table1,
-        only_in_table2,
+        only_in_sql_table,
+        only_in_dataverse_table,
         common_rows_same_values,
         common_rows_diff_values,
     )
 
 
 @bp_compare_tables.route(route="comparetbl")
-def compare_tables(req: func.HttpRequest) -> func.HttpResponse:
+def comparetbl(req: func.HttpRequest) -> func.HttpResponse:
     try:
         req_body = req.get_json()
-        table1 = req_body.get("Table1", [])
-        table2 = req_body.get("Table2", [])
+        sql_table = req_body.get("sql_table", [])
+        dataverse_table = req_body.get("dataverse_table", [])
 
         (
-            only_in_table1,
-            only_in_table2,
+            only_in_sql_table,
+            only_in_dataverse_table,
             common_rows_same_values,
             common_rows_diff_values,
-        ) = compare_tables(table1, table2)
+        ) = compare_tables(sql_table, dataverse_table)
 
         result = {
-            "only_in_table1": only_in_table1,
-            "only_in_table2": only_in_table2,
+            "only_in_sql_table": only_in_sql_table,
+            "only_in_dataverse_table": only_in_dataverse_table,
             "common_rows_same_values": common_rows_same_values,
             "common_rows_diff_values": common_rows_diff_values,
         }
--- function_app.py
+++ function_app.py
@@ -1,7 +1,9 @@
 import azure.functions as func
 import logging
 from bp_http_parsecsv import bp_parse_csv
+from bp_http_comparetables import bp_compare_tables
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 app.register_functions(bp_parse_csv)
+app.register_functions(bp_compare_tables)
