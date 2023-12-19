b9b64c8de29f3227342c874f565a504da670b45f
Ensui Chen <echen770@gmail.com>
2023-12-19 12:24:58 -0800

    added unmatched logic for office titles

--- bp_http_setofficetitles.py
+++ bp_http_setofficetitles.py
@@ -4,39 +4,30 @@ import json
 import base64
 import traceback
 import io
-import pandas as pd
 
 bp_set_office_title = func.Blueprint()
 
 
 def process_office_titles(office_title_formulas, contest_snapshot):
     office_titles = []
+    unmatched_entries = []
 
     for contest_entry in contest_snapshot:
         office_id = contest_entry["office_id"]
-        matching_formula = next(
-            (
-                item["formula"]
-                for item in office_title_formulas
-                if item["office_id"] == office_id
-            ),
+
+        matching_formula_entry = next(
+            (item for item in office_title_formulas if item["office_id"] == office_id),
             None,
         )
 
-        # if matching_formula is not None:
-        #     concat_office_title = (
-        #         matching_formula.replace("<bt1>", contest_entry["bt1"])
-        #         .replace("<bt2>", contest_entry["bt2"])
-        #         .replace("<bt3>", contest_entry["bt3"])
-        #     )
-
-        if matching_formula is not None:
+        if matching_formula_entry is not None:
             bt1 = contest_entry["bt1"] if contest_entry["bt1"] is not None else ""
             bt2 = contest_entry["bt2"] if contest_entry["bt2"] is not None else ""
             bt3 = contest_entry["bt3"] if contest_entry["bt3"] is not None else ""
 
             concat_office_title = (
-                matching_formula.replace("<bt1>", bt1)
+                matching_formula_entry["formula"]
+                .replace("<bt1>", bt1)
                 .replace("<bt2>", bt2)
                 .replace("<bt3>", bt3)
             )
@@ -44,12 +35,23 @@ def process_office_titles(office_title_formulas, contest_snapshot):
             office_title = {
                 "contest_snapshot_guid": contest_entry["contest_snapshot_guid"],
                 "concat_office_title": concat_office_title,
+                "concat_formula": matching_formula_entry["formula"],
                 "office_id": office_id,
             }
 
             office_titles.append(office_title)
 
-    return office_titles
+        else:
+            unmatched_entry = {
+                "unmatched_contest_snapshot_guid": contest_entry[
+                    "contest_snapshot_guid"
+                ],
+                "unmatched_office_id": office_id,
+            }
+
+            unmatched_entries.append(unmatched_entry)
+
+    return office_titles, unmatched_entries
 
 
 @bp_set_office_title.route(route="setofficetitle")
@@ -66,7 +68,14 @@ def set_office_title(req: func.HttpRequest) -> func.HttpResponse:
                 status_code=400,
             )
 
-        result = process_office_titles(office_title_formulas, contest_snapshot)
+        matched_titles, unmatched_entries = process_office_titles(
+            office_title_formulas, contest_snapshot
+        )
+
+        result = {
+            "office_titles": matched_titles,
+            "unmatched_entries": unmatched_entries,
+        }
 
         return func.HttpResponse(
             json.dumps(result), mimetype="application/json", status_code=200
